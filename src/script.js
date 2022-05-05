f19808bccce10b832207b7cfcd53f5b64c62b7ac
Abdallah <abdallahabouchahine@cmail.carleton.ca>
2022-05-05 06:36:33 -0400

    added navbar & progression bar

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import { updateProgress, progressBar } from "./Progress.js";
 import * as THREE from "three";
 import * as dat from "lil-gui";
 import gsap from "gsap";
@@ -7,10 +8,10 @@ import gsap from "gsap";
  */
 
 const gui = new dat.GUI();
-gui.close();
+gui.hide();
 
 const parameters = {
-  materialColor: "#ffeded",
+	materialColor: "#ffeded",
 };
 
 gui.addColor(parameters, "materialColor");
@@ -30,16 +31,16 @@ const scene = new THREE.Scene();
 
 const loadingManager = new THREE.LoadingManager();
 loadingManager.onStart = () => {
-  console.log("loadingManager: loading started");
+	console.log("loadingManager: loading started");
 };
 loadingManager.onLoad = () => {
-  console.log("loadingManager: loading finished");
+	console.log("loadingManager: loading finished");
 };
 loadingManager.onProgress = () => {
-  console.log("loadingManager: loading progressing");
+	console.log("loadingManager: loading progressing");
 };
 loadingManager.onError = () => {
-  console.log("loadingManager: loading error");
+	console.log("loadingManager: loading error");
 };
 
 const textureLoader = new THREE.TextureLoader(loadingManager);
@@ -47,16 +48,16 @@ const textureLoader = new THREE.TextureLoader(loadingManager);
 // const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
 // const colorTexture = textureLoader.load('/textures/checkerboard-2x2.png')
 const normalTexture = textureLoader.load(
-  "/textures/pebbleNormalMap.jpeg",
-  () => {
-    console.log("textureLoader: loading finished");
-  },
-  () => {
-    console.log("textureLoader: loading progressing");
-  },
-  () => {
-    console.log("textureLoader: loading error");
-  }
+	"/textures/pebbleNormalMap.jpeg",
+	() => {
+		console.log("textureLoader: loading finished");
+	},
+	() => {
+		console.log("textureLoader: loading progressing");
+	},
+	() => {
+		console.log("textureLoader: loading error");
+	}
 );
 const coneNormalTexture = textureLoader.load("/textures/coneNormalMap.jpeg");
 /**
@@ -67,39 +68,39 @@ const coneNormalTexture = textureLoader.load("/textures/coneNormalMap.jpeg");
 const sphereGeometry = new THREE.SphereBufferGeometry(0.5, 64, 64);
 console.log(sphereGeometry.attributes);
 const sphereMaterial = new THREE.MeshStandardMaterial({
-  color: 0x292929,
-  metalness: 2,
-  roughness: 0.8,
-  normalMap: normalTexture,
+	color: 0x292929,
+	metalness: 2,
+	roughness: 0.8,
+	normalMap: normalTexture,
 });
 
 // Torus
 const torusGeometry = new THREE.TorusGeometry(1, 0.4, 16, 60);
 console.log(torusGeometry.attributes);
 const torusMaterial = new THREE.MeshStandardMaterial({
-  color: 0x292929,
-  metalness: 2,
-  roughness: 0.8,
-  normalMap: normalTexture,
+	color: 0x292929,
+	metalness: 2,
+	roughness: 0.8,
+	normalMap: normalTexture,
 });
 
 // Torus Knot
 const torusKnotGeometry = new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16);
 console.log(torusKnotGeometry.attributes);
 const torusKnotMaterial = new THREE.MeshStandardMaterial({
-  color: 0x292929,
-  metalness: 2,
-  roughness: 0.8,
-  normalMap: normalTexture,
+	color: 0x292929,
+	metalness: 2,
+	roughness: 0.8,
+	normalMap: normalTexture,
 });
 
 const octahedronGeometry = new THREE.OctahedronGeometry(0.3, 0);
 console.log(octahedronGeometry.attributes);
 const octahedronMaterial = new THREE.MeshStandardMaterial({
-  color: 0x292929,
-  metalness: 2,
-  roughness: 0.8,
-  normalMap: normalTexture,
+	color: 0x292929,
+	metalness: 2,
+	roughness: 0.8,
+	normalMap: normalTexture,
 });
 
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
@@ -149,21 +150,21 @@ sphereMaterialGUI.add(sphereMaterial, "roughness").min(0).max(1).step(0.01);
 const particlesCount = 4500;
 const positions = new Float32Array(particlesCount * 3);
 for (let i = 0; i < particlesCount; i++) {
-  positions[i * 3 + 0] = (Math.random() - 0.5) * 20;
-  positions[i * 3 + 1] = objectsDistance - Math.random() * 24;
-  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+	positions[i * 3 + 0] = (Math.random() - 0.5) * 20;
+	positions[i * 3 + 1] = objectsDistance - Math.random() * 24;
+	positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
 }
 const particlesGeometry = new THREE.BufferGeometry();
 particlesGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3)
+	"position",
+	new THREE.BufferAttribute(positions, 3)
 );
 
 //Particle Material
 const particleMaterial = new THREE.PointsMaterial({
-  color: 0xffffff,
-  sizeAttenuation: true,
-  size: 0.01,
+	color: 0xffffff,
+	sizeAttenuation: true,
+	size: 0.01,
 });
 
 //Points
@@ -187,11 +188,11 @@ mainLight.add(pointLight, "intensity").min(0).max(10).step(0.01);
 
 // Main Point Light 1 Color GUI
 const mainLightColor = {
-  color: 0xff00000,
+	color: 0xff00000,
 };
 mainLight
-  .addColor(mainLightColor, "color")
-  .onChange(() => pointLight.color.set(mainLightColor.color));
+	.addColor(mainLightColor, "color")
+	.onChange(() => pointLight.color.set(mainLightColor.color));
 
 // Point Light 2
 const pointLight2 = new THREE.PointLight(0x782097, 9.75);
@@ -211,31 +212,31 @@ pointLight2GUI.add(pointLight2, "intensity").min(0).max(10).step(0.01);
 
 // Main Point Light 1 Color GUI
 const pointLight2Color = {
-  color: 0xff00000,
+	color: 0xff00000,
 };
 pointLight2GUI.addColor(pointLight2Color, "color").onChange(() => {
-  pointLight2.color.set(pointLight2Color.color);
+	pointLight2.color.set(pointLight2Color.color);
 });
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+	width: window.innerWidth,
+	height: window.innerHeight,
 };
 window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+	// Update sizes
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
+	// Update camera
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+	// Update renderer
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 /**
@@ -249,10 +250,10 @@ scene.add(cameraGroup);
 
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  35,
-  sizes.width / sizes.height,
-  0.1,
-  100
+	35,
+	sizes.width / sizes.height,
+	0.1,
+	100
 );
 camera.position.z = 5;
 cameraGroup.add(camera);
@@ -261,7 +262,7 @@ cameraGroup.add(camera);
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+	canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -273,20 +274,20 @@ let scrollY = window.scrollY;
 let currentSection = 0;
 
 window.addEventListener("scroll", () => {
-  scrollY = window.scrollY;
+	scrollY = window.scrollY;
 
-  const newSection = Math.round(scrollY / sizes.height);
-  if (newSection != currentSection) {
-    currentSection = newSection;
+	const newSection = Math.round(scrollY / sizes.height);
+	if (newSection != currentSection) {
+		currentSection = newSection;
 
-    gsap.to(sectionMeshes[currentSection].rotation, {
-      duration: 3,
-      ease: "power2.inOut",
-      x: "+=3",
-      y: "+=3",
-    });
-  }
-  console.log(newSection);
+		gsap.to(sectionMeshes[currentSection].rotation, {
+			duration: 3,
+			ease: "power2.inOut",
+			x: "+=3",
+			y: "+=3",
+		});
+	}
+	console.log(newSection);
 });
 /**
  * Cursor
@@ -297,10 +298,10 @@ cursor.x = 0;
 cursor.y = 0;
 
 window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = event.clientY / sizes.height - 0.5;
+	cursor.x = event.clientX / sizes.width - 0.5;
+	cursor.y = event.clientY / sizes.height - 0.5;
 
-  console.log(cursor);
+	console.log(cursor);
 });
 /**
  * Animate
@@ -309,30 +310,30 @@ const clock = new THREE.Clock();
 let previousTime = 0;
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime;
-  previousTime = elapsedTime;
+	const elapsedTime = clock.getElapsedTime();
+	const deltaTime = elapsedTime - previousTime;
+	previousTime = elapsedTime;
 
-  // Animate camera
-  camera.position.y = scrollY * -0.01;
+	// Animate camera
+	camera.position.y = scrollY * -0.01;
 
-  const parallaxX = cursor.x * 0.3;
-  const parallaxY = -cursor.y * 0.3;
+	const parallaxX = cursor.x * 0.3;
+	const parallaxY = -cursor.y * 0.3;
 
-  cameraGroup.position.x +=
-    (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
-  cameraGroup.position.y +=
-    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
+	cameraGroup.position.x +=
+		(parallaxX - cameraGroup.position.x) * 5 * deltaTime;
+	cameraGroup.position.y +=
+		(parallaxY - cameraGroup.position.y) * 5 * deltaTime;
 
-  // Render
-  renderer.render(scene, camera);
-  sphere.rotation.y += 0.5 * deltaTime;
-  torus.rotation.z += -0.3 * deltaTime;
-  torusKnot.rotation.z += -0.3 * deltaTime;
-  octahedron.rotation.z += -0.2 * deltaTime;
+	// Render
+	renderer.render(scene, camera);
+	sphere.rotation.y += 0.5 * deltaTime;
+	torus.rotation.z += -0.3 * deltaTime;
+	torusKnot.rotation.z += -0.3 * deltaTime;
+	octahedron.rotation.z += -0.2 * deltaTime;
 
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
+	// Call tick again on the next frame
+	window.requestAnimationFrame(tick);
 };
 
 tick();
